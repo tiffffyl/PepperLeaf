cf80942ee17ee21335001d78f803f8a9b718273a
leety <tiffany.lee2@student.unimelb.edu.au>
2024-05-27 06:21:02 +1000

    Improved checkout page

--- js/checkout.js
+++ js/checkout.js
@@ -1,31 +1,29 @@
-let currentSection = 0;
+let currentPage = 0;
+const pages = document.querySelectorAll('.page');
 
-function nextPage(sectionIndex) {
-    const formContainer = document.querySelector('.form-container');
-    formContainer.style.transform = `translateX(-${sectionIndex * 100}%)`;
-    currentSection = sectionIndex;
-    toggleBackButton();
-}
-
-function goBack(sectionIndex) {
-    const formContainer = document.querySelector('.form-container');
-    formContainer.style.transform = `translateX(-${sectionIndex * 100}%)`;
-    currentSection = sectionIndex;
-    toggleBackButton();
-}
-
-function toggleBackButton() {
-    const backBtn1 = document.getElementById('backBtn1');
-    const backBtn2 = document.getElementById('backBtn2');
-    const backBtn3 = document.getElementById('backBtn3');
-    
-    backBtn1.classList.add('hidden');
-    backBtn2.classList.add('hidden');
-    backBtn3.classList.add('hidden');
-
-    if (currentSection === 1) {
-        backBtn2.classList.remove('hidden');
-    } else if (currentSection === 2) {
-        backBtn3.classList.remove('hidden');
+function nextPage() {
+    if (window.innerWidth < 768 && currentPage < pages.length - 1) {
+        pages[currentPage].classList.remove('active');
+        currentPage++;
+        pages[currentPage].classList.add('active');
+    } else if (window.innerWidth >= 768 && currentPage < pages.length - 1) {
+        pages[currentPage].classList.remove('active');
+        currentPage++;
+        pages[currentPage].classList.add('active');
+        pages[currentPage - 1].style.transform = 'translateX(-100%)';
     }
 }
+
+function prevPage() {
+    if (window.innerWidth < 768 && currentPage > 0) {
+        pages[currentPage].classList.remove('active');
+        currentPage--;
+        pages[currentPage].classList.add('active');
+    } else if (window.innerWidth >= 768 && currentPage > 0) {
+        pages[currentPage].classList.remove('active');
+        currentPage--;
+        pages[currentPage].classList.add('active');
+        pages[currentPage + 1].style.transform = 'translateX(100%)';
+    }
+}
+
