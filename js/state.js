c2330bd6d0efe123deb6e42fa2a716bceaaa9eb0
Tiffany <leety@student.unimelb.edu.au>
2024-05-30 00:11:50 +1000

    Set up about-us page - imported illustration and images for about us page - added parallax effect on about us page elements - fixed ordering page mobile responsivity - made home page mobile responsive - made footer mobile responsive - made recipe page mobile responive

--- js/state.js
+++ js/state.js
@@ -1,1 +1,13 @@
+//Keep track of hamburger menu
+var isMenuOpen = false;
 
+function toggleMenu() {
+  const navbarLinks = document.getElementById('navbarLinks');
+  navbarLinks.classList.toggle('show');
+  
+  document.querySelector('.navbar-container').classList.toggle('nav-open');
+  isMenuOpen = !isMenuOpen;
+  
+  var iconSrc = isMenuOpen ? '/Assets/icons/close-hamburger.svg' : '/Assets/icons/hamburger.svg';
+  document.querySelector('#hamburgerMenu img').setAttribute('src', iconSrc);
+}
